60e402219b00e45f58fed0044e1673f3107a7297
srnyx <25808801+srnyx@users.noreply.github.com>
2024-06-21 18:58:08 -0400

    Bump version (1.2.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "xyz.srnyx"
-version = "1.1.4"
+version = "1.2.0"
 description = "A Gradle plugin to simplify the process of creating projects"
 val projectId: String = "gradle-galaxy"
 val vcs: String = "github.com/srnyx/$projectId"
