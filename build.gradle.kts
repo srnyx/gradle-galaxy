396daaf887a0f588175a058cdd5d7c6a9f125678
srnyx <25808801+srnyx@users.noreply.github.com>
2024-08-23 15:17:32 -0400

    Update Kotlin JVM plugin
    1.9.22 -> 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     `java-gradle-plugin`
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.0.20"
     id("com.gradle.plugin-publish") version "1.2.1"
     id("org.jetbrains.dokka") version "1.9.20"
 }
