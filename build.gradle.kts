6fc01830ae4377760bd9a0d4dbbbc4be80750b74
srnyx <25808801+srnyx@users.noreply.github.com>
2026-06-24 21:09:01 -0400

    Bump version (3.0.1 -> 3.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "xyz.srnyx"
-version = providers.environmentVariable("VERSION").orNull?.takeIf(String::isNotBlank) ?: "3.0.1"
+version = providers.environmentVariable("VERSION").orNull?.takeIf(String::isNotBlank) ?: "3.1.0"
 description = "A Gradle plugin to simplify the process of creating projects"
 val projectId: String = "gradle-galaxy"
 val vcs: String = "github.com/srnyx/$projectId"
